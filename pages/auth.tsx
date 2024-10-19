2a6e3508fa20d775a9d09e4e4c18e88626cf7986
Jzheng-prog <74681328+Jzheng-prog@users.noreply.github.com>
2024-10-19 00:52:48 -0400

    first commit

---
+++ pages/auth.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Auth = () => {
+  return (
+    <div className='relative h-full w-full bg-[url("/images/hero.jpg")] bg-no-repeat bg-fixed bg-cover'>
+      <div className='bg-black w-full h-full lg:bg-opacity-50'> 
+        <nav className='px-12 py-5'>
+            <img src="/images/Netflix-logo.png" alt="" className='h-[100px]'/>
+        </nav>
+      </div>
+    </div>
+  )
+}
+
+export default Auth
