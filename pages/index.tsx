6a2787eb2340192db182b4fd5627be3729178432
Jzheng-prog <zhengjohn017@gmail.com>
2024-10-23 00:52:50 -0400

    profile section added, authentication + css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,27 @@
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+import { redirect } from "next/dist/server/api-utils";
 
+export async function getServerSideProps(context: NextPageContext){
+  const session = await getSession(context)
+
+  if(!session){
+    return {
+      redirect:{
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+  return {
+    props:{}
+  }
+}
 export default function Home() {
   return (
     <div>
-      home
+      <h1 className="text-green-500">NetFlic Clone</h1>
+      <button onClick={()=>signOut()} className="h-10 w-full bg-white">Logout</button>
     </div>
   );
 }
